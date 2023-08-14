218ca2b166c9f9a6cfb7c4042636b221e81176d1
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-08-14 17:24:46 +0200

    remove temporarily  internationalization

--- next.config.js
+++ next.config.js
@@ -2,15 +2,15 @@
 const nextConfig = {
    optimizeFonts: true,
    crossOrigin: "use-credentials",
-   i18n: {
-      // These are all the locales you want to support in
-      // your application
-      locales: [ "fr"],
-      // This is the default locale you want to be used when visiting
-      // a non-locale prefixed path e.g. `/hello`
-      defaultLocale: "fr",
-
-   },
+   // i18n: {
+   //    // These are all the locales you want to support in
+   //    // your application
+   //    locales: [ "fr"],
+   //    // This is the default locale you want to be used when visiting
+   //    // a non-locale prefixed path e.g. `/hello`
+   //    defaultLocale: "fr",
+   //
+   // },
 }
 
 module.exports = nextConfig
