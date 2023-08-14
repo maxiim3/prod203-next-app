0f79c228e0bf687bf3581c5f1c08980ba107a839
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-08-14 13:15:25 +0200

    add video and logo assets | Video playing

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import {css} from "@/styled-system/css"
 
 function VideoBanner() {
-   return <section aria-label={"Video hero banner"} className="hero min-h-screen">
-      <video controls>
-         <source src="{/*Path*/}" type="video/mp4" media="(min-width: 800px)" />
-         <source src="{/*Path*/}" type="video/mp4" media="(min-width: 500px) and (max-width: 799px)" />
-         <source src="{/*Path*/}" type="video/mp4" media="(max-width: 499px)" />
-         <source src="/*Path*/" type="video/mp4" />
+   return <section aria-label={"Video hero banner"} className="hero w-screen h-screen">
+      <video autoPlay={true} muted={true} loop={true} controls={false}
+             className={"object-cover object-center w-[100%] h-[100vh]"}>
+         <source src="/assets/video/pexels-c-technical-7095057%20(540p).mp4" type="video/mp4" />
+         <source src="/assets/video/pexels-c-technical-7095057%20(720p).mp4" type="video/mp4"
+                 media="(min-width: 680px) and (max-width: 1080px)" />
+         <source src="/assets/video/pexels-c-technical-7095057%20(1080p).mp4" type="video/mp4"
+                 media="(min-width: 1080px) and (max-width: 1439px)" />
+         <source src="/assets/video/pexels-c-technical-7095057%20(2160p).mp4" type="video/mp4"
+                 media="(min-width: 1440px)" />
       </video>
 
-      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-overlay bg-opacity-20"></div>
       <div className="hero-content text-center text-neutral-content">
          <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">PROD203</h1>
--- components/StickyNavBar.tsx
+++ components/StickyNavBar.tsx
@@ -1,23 +1,16 @@
 import React from "react"
 
 export default function StickyNavBar() {
-   return <div className="navbar bg-base-100">
+   return (
+      <header className="navbar bg-base-100 fixed top-0 left-o">
+         <nav className="flex-none">
+            <ul className="menu menu-horizontal px-1">
+               <li><a>Accueilnpm install i18next --save</a></li>
+               <li><a>Home</a></li>
+               <li><a>Home</a></li>
+               <li><a>Home</a></li>
 
-      <div className="flex-none">
-         <ul className="menu menu-horizontal px-1">
-            <li><a>Link</a></li>
-            <li>
-               <details>
-                  <summary>
-                     Parent
-                  </summary>
-                  <ul className="p-2 ">
-                     <li><a>Link 1</a></li>
-                     <li><a>Link 2</a></li>
-                  </ul>
-               </details>
-            </li>
-         </ul>
-      </div>
-   </div>
+            </ul>
+         </nav>
+      </header>)
 }
