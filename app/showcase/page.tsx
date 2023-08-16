f83b80cad698e8bf6c304e3590a77dd14baac8f7
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-08-16 13:58:33 +0200

    Page Title Animation

--- app/showcase/page.tsx
+++ app/showcase/page.tsx
@@ -1,12 +1,25 @@
+import {SectionTitle} from "@/app/UI"
+import {ShadowTitleAnimation} from "@/app/showcase/ShadowTitleAnimation"
 import React from "react"
-import {BuildingPage} from "@/components/molecule/BuildingPage"
+import {twMerge} from "tailwind-merge"
 
 const Showcase = () => {
    return (
-      <main className={'w-screen h-screen pt-24'}>
-         <BuildingPage title={'Showcase'} />
+      <main className={"h-screen w-screen pt-24"}>
+         <PageHeadingParticule title={"Showcase"} />
       </main>
    )
 }
 
 export default Showcase
+
+const PageHeadingParticule = ({title}: {title: string}) => (
+   <section className={"relative"}>
+      <SectionTitle
+         as={"h1"}
+         className={twMerge("mx-auto animate-reveal px-0")}>
+         {title}
+      </SectionTitle>
+      <ShadowTitleAnimation title={title} />
+   </section>
+)
