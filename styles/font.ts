99806a11684233c2fad73122d2be1bc1509faf81
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-08-24 13:31:18 +0200

    updated typo : Font from Next/Font using Google Font

---
+++ styles/font.ts
@@ -0,0 +1,21 @@
+import {Cormorant_SC, Manrope, Poppins} from 'next/font/google'
+
+const poppins = Poppins({
+   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+   subsets: ['latin-ext'],
+   variable: '--font-poppins',
+})
+
+const cormorant = Cormorant_SC({
+   weight: ['300', '400', '500', '600', '700'],
+   subsets: ['latin-ext'],
+   variable: '--font-cormorant',
+})
+
+const manrope = Manrope({
+   weight: ['300', '400', '500', '600', '700', '800'],
+   subsets: ['latin-ext'],
+   variable: '--font-manrope',
+})
+
+export {poppins}
