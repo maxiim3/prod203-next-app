c7ea3ac473309de65de8c8968411fa9cebd9f36c
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-08-25 12:36:30 +0200

    Added Issue

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,65 @@ import Icons from '@/lib/Icons'
 import React, {PropsWithChildren, Suspense} from 'react'
 import {twMerge} from 'tailwind-merge'
 
+function ScrollButton() {
+   return (
+      <a
+         href={'/#services'}
+         className={twMerge(
+            ' fixed bottom-4 left-1/2 rounded-xl p-3',
+            'border-none bg-base-100/20 backdrop-blur-sm',
+            'motion-safe:animate-revealFromBottom',
+            'hover:bg-base-100',
+            'group z-50'
+         )}>
+         <Icons.ArrowFull
+            className={
+               'animate-bounce text-4xl text-primary/75 group-hover:scale-105 group-hover:animate-none'
+            }
+         />
+      </a>
+   )
+}
+
+function LandingVideo() {
+   return (
+      <section
+         id={'hero'}
+         aria-label={'Video hero banner'}
+         className="absolute top-0 h-screen w-screen">
+         <video
+            autoPlay={true}
+            muted={true}
+            loop={true}
+            controls={false}
+            className={twMerge(
+               'h-[100vh] w-[100%] object-cover object-center',
+               'motion-safe:animate-[scaleAndFade_50ms_ease-out_both]'
+            )}>
+            <source
+               src="/assets/video/pexels-c-technical-7095057%20(540p).mp4"
+               type="video/mp4"
+            />
+            <source
+               src="/assets/video/pexels-c-technical-7095057%20(720p).mp4"
+               type="video/mp4"
+               media="(min-width: 680px) and (max-width: 1080px)"
+            />
+            <source
+               src="/assets/video/pexels-c-technical-7095057%20(1080p).mp4"
+               type="video/mp4"
+               media="(min-width: 1080px) and (max-width: 1439px)"
+            />
+            <source
+               src="/assets/video/pexels-c-technical-7095057%20(2160p).mp4"
+               type="video/mp4"
+               media="(min-width: 1440px)"
+            />
+         </video>
+      </section>
+   )
+}
+
 /* NOTE about animation
  * Animation is done within TailwindCSS - IN LINE -> animate-[keyframe...custom] - for the element of this page : Header apparition, title and subtitle apparition
  * The Button is animated in tailwind.config.js -> animate-revealFromBottom
@@ -12,44 +71,15 @@ import {twMerge} from 'tailwind-merge'
  * */
 export default function Home() {
    return (
-      <main>
-         <section
-            id={'hero'}
-            aria-label={'Video hero banner'}
-            className="hero h-screen w-screen">
-            <video
-               autoPlay={true}
-               muted={true}
-               loop={true}
-               controls={false}
-               className={twMerge(
-                  'h-[100vh] w-[100%] object-cover object-center',
-                  'motion-safe:animate-[scaleAndFade_50ms_ease-out_both]'
-               )}>
-               <source
-                  src="/assets/video/pexels-c-technical-7095057%20(540p).mp4"
-                  type="video/mp4"
-               />
-               <source
-                  src="/assets/video/pexels-c-technical-7095057%20(720p).mp4"
-                  type="video/mp4"
-                  media="(min-width: 680px) and (max-width: 1080px)"
-               />
-               <source
-                  src="/assets/video/pexels-c-technical-7095057%20(1080p).mp4"
-                  type="video/mp4"
-                  media="(min-width: 1080px) and (max-width: 1439px)"
-               />
-               <source
-                  src="/assets/video/pexels-c-technical-7095057%20(2160p).mp4"
-                  type="video/mp4"
-                  media="(min-width: 1440px)"
-               />
-            </video>
-
-            <main className="hero-overlay relative bg-opacity-20"></main>
-            <article className="hero-content text-center text-neutral-content">
-               <article className={'mx-auto select-none text-primary '}>
+      <main className={'relative'}>
+         <ScrollButton />
+         <header>
+            <LandingVideo />
+            <section className="h-screen text-center text-neutral-content">
+               <article
+                  className={
+                     'mx-auto flex h-full select-none flex-col items-center justify-center text-primary'
+                  }>
                   <h1
                      className={twMerge(
                         `font-poppins font-bold uppercase`,
@@ -65,56 +95,44 @@ export default function Home() {
                      Façonnons l’Art du son
                   </p>
                </article>
+            </section>
+         </header>
 
-               <a
-                  href={'/#services'}
-                  className={twMerge(
-                     'btn btn-primary absolute bottom-4',
-                     'border-none bg-base-100/50 backdrop-blur-sm',
-                     'motion-safe:animate-revealFromBottom',
-                     'hover:bg-base-100',
-                     'group'
-                  )}>
-                  <Icons.Arrow
-                     className={
-                        'animate-bounce text-2xl text-primary/75 group-hover:scale-105 group-hover:animate-none'
-                     }
-                  />
-               </a>
-            </article>
-         </section>
-         <SectionTemplate
-            id={'services'}
-            ariaLabel={'Nos Services'}
-            className={''}
-            title={'Nos Services'}>
-            <article className="grid w-full grid-cols-2 place-content-center place-items-center gap-2 sm:grid-cols-4 md:gap-6">
-               {services.map((service, index) => (
-                  <span
-                     key={service.title}
-                     className={twMerge(
-                        'flex w-full flex-col items-center justify-center place-self-center p-2 sm:w-1/2 ',
-                        index < services.length - 2 ? 'grid-col-auto' : 'grid-col-span-2'
-                     )}>
-                     <service.Icon className="text-4xl text-primary sm:text-5xl md:text-6xl" />
-                     <h3 className="text-center text-base text-primary text-balance md:text-lg">
-                        {service.title}
-                     </h3>
-                     {/*<p className="text-base text-center">{service.content}</p>*/}
-                  </span>
-               ))}
-            </article>
-         </SectionTemplate>
-         <SectionTemplate
-            title={'Clients'}
-            id={'clients'}
-            ariaLabel={'Nos Clients'}
-            className={'w-screen bg-primary/80 text-base-100 [&>*]:text-base-100'}>
-            <Suspense fallback={<p>Loading...</p>}>
-               <AnimatedClientBanner />
-               <StaticClientBanner />
-            </Suspense>
-         </SectionTemplate>
+         <div className="relative snap-y snap-mandatory">
+            <SectionTemplate
+               id={'services'}
+               ariaLabel={'Nos Services'}
+               className={''}
+               title={'Nos Services'}>
+               <article className="grid w-full grid-cols-2 place-content-center place-items-center gap-2 sm:grid-cols-4 md:gap-6">
+                  {services.map((service, index) => (
+                     <span
+                        key={service.title}
+                        className={twMerge(
+                           'flex w-full flex-col items-center justify-center place-self-center p-2 sm:w-1/2 ',
+                           index < services.length - 2 ? 'grid-col-auto' : 'grid-col-span-2'
+                        )}>
+                        <service.Icon className="text-4xl text-primary sm:text-5xl md:text-6xl" />
+                        <h3 className="text-center text-base text-primary text-balance md:text-lg">
+                           {service.title}
+                        </h3>
+                        {/*<p className="text-base text-center">{service.content}</p>*/}
+                     </span>
+                  ))}
+               </article>
+            </SectionTemplate>
+
+            <SectionTemplate
+               title={'Clients'}
+               id={'clients'}
+               ariaLabel={'Nos Clients'}
+               className={'w-screen bg-primary/80 text-base-100 [&>*]:text-base-100'}>
+               <Suspense fallback={<p>Loading...</p>}>
+                  <AnimatedClientBanner />
+                  <StaticClientBanner />
+               </Suspense>
+            </SectionTemplate>
+         </div>
       </main>
    )
 }
@@ -128,14 +146,17 @@ interface SectionTemplateProps extends PropsWithChildren {
 
 const SectionTemplate = ({children, className, ariaLabel, title, id}: SectionTemplateProps) => (
    <section
-      className={twMerge(
-         'min-h-lg my-40 flex w-screen flex-col items-center justify-center gap-12 bg-base-200 px-8 py-20',
-         className
-      )}
+      className={twMerge(' flex min-h-screen w-screen snap-center items-center')}
       aria-label={ariaLabel}
       id={id}>
-      <TitleH2 className={'font-semibold'}>{title}</TitleH2>
-      {children}
+      <main
+         className={twMerge(
+            'min-h-xl my-32 w-full flex-col items-center justify-center gap-12 overflow-hidden bg-base-200 py-32',
+            className
+         )}>
+         <TitleH2 className={'font-semibold'}>{title}</TitleH2>
+         {children}
+      </main>
    </section>
 )
 
