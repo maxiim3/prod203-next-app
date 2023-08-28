1523bab1c1579632236c21e0d5bc7a23c3adbef1
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-08-28 16:04:04 +0200

    add section to main home page

--- components/organism/DropDownMenu.tsx
+++ components/organism/DropDownMenu.tsx
@@ -1,19 +1,19 @@
-'use client'
+"use client"
 
-import BurgerButton from '@/components/molecule/BurgerButton'
-import MenuPortal from '@/components/organism/MenuPortal'
-import {useMobileNavigation} from '@/components/organism/MobileNavigationProvider'
-import React from 'react'
-import {createPortal} from 'react-dom'
+import BurgerButton from "@/components/molecule/BurgerButton"
+import MenuPortal from "@/components/organism/MenuPortal"
+import {useMobileNavigation} from "@/components/organism/MobileNavigationProvider"
+import React from "react"
+import {createPortal} from "react-dom"
 
 export default function DropDownMenu() {
-   const {modalIsOpen} = useMobileNavigation()
+	const {modalIsOpen} = useMobileNavigation()
 
-   return (
-      <div className="flex-none md:hidden">
-         {/*Burger Button Trigger*/}
-         <BurgerButton />
-         {modalIsOpen && createPortal(<MenuPortal />, document.body!)}
-      </div>
-   )
+	return (
+		<div className="flex items-center md:hidden">
+			{/*Burger Button Trigger*/}
+			<BurgerButton />
+			{modalIsOpen && createPortal(<MenuPortal />, document.body!)}
+		</div>
+	)
 }
--- components/organism/MenuPortal.tsx
+++ components/organism/MenuPortal.tsx
@@ -1,47 +1,47 @@
-'use client'
+"use client"
 
-import NavItem from '@/components/atom/NavItem'
-import {NavListStack} from '@/components/layout/NavListStack'
-import LanguageSelection from '@/components/molecule/LanguageSelection'
-import {useMobileNavigation} from '@/components/organism/MobileNavigationProvider'
-import ROUTES from '@/lib/ROUTES'
-import {motion} from 'framer-motion'
-import {nanoid} from 'nanoid'
-import Link from 'next/link'
-import {usePathname} from 'next/navigation'
-import React from 'react'
+import NavItem from "@/components/atom/NavItem"
+import {NavListStack} from "@/components/layout/NavListStack"
+import LanguageSelection from "@/components/molecule/LanguageSelection"
+import {useMobileNavigation} from "@/components/organism/MobileNavigationProvider"
+import ROUTES from "@/lib/ROUTES"
+import {motion} from "framer-motion"
+import {nanoid} from "nanoid"
+import Link from "next/link"
+import {usePathname} from "next/navigation"
+import React from "react"
 
 const MenuPortal = () => {
-   const {handleCloseMenu, modalIsOpen} = useMobileNavigation()
-   const pathName = usePathname()
+	const {handleCloseMenu, modalIsOpen} = useMobileNavigation()
+	const pathName = usePathname()
 
-   return (
-      <dialog
-         className={'fixed left-0 top-0 h-screen w-screen md:hidden'}
-         open={modalIsOpen}>
-         <motion.article className={'h-full w-full bg-base-100/95 backdrop-blur-lg md:hidden'}>
-            <nav className={'flex h-full w-full flex-col items-end justify-center gap-16  px-4 '}>
-               <NavListStack className="flex-col items-end gap-16 sm:gap-24">
-                  {ROUTES.map(route => (
-                     <NavItem
-                        key={nanoid()}
-                        className={`duration-250 text-5xl motion-safe:transition-all sm:text-6xl`}>
-                        <Link
-                           onClick={() => pathName === route.path && handleCloseMenu()}
-                           href={route.path}>
-                           {route.name}
-                        </Link>
-                     </NavItem>
-                  ))}
-               </NavListStack>
-               <LanguageSelection
-                  className={'text-2xl text-primary sm:text-4xl'}
-                  onClick={handleCloseMenu}
-                  large
-               />
-            </nav>
-         </motion.article>
-      </dialog>
-   )
+	return (
+		<dialog
+			className={"fixed left-0 top-0 h-screen w-screen md:hidden"}
+			open={modalIsOpen}>
+			<motion.article className={"h-full w-full bg-base-100/95 backdrop-blur-lg md:hidden"}>
+				<nav className={"flex h-full w-full flex-col items-end justify-center gap-16  px-12"}>
+					<NavListStack className="flex-col items-end gap-16 sm:gap-24">
+						{ROUTES.map(route => (
+							<NavItem
+								key={nanoid()}
+								className={`duration-250 text-5xl motion-safe:transition-all sm:text-6xl`}>
+								<Link
+									onClick={() => pathName === route.path && handleCloseMenu()}
+									href={route.path}>
+									{route.name}
+								</Link>
+							</NavItem>
+						))}
+					</NavListStack>
+					<LanguageSelection
+						className={"text-2xl text-primary sm:text-4xl"}
+						onClick={handleCloseMenu}
+						large
+					/>
+				</nav>
+			</motion.article>
+		</dialog>
+	)
 }
 export default MenuPortal
