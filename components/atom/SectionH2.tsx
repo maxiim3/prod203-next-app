c9018b0a6d8b7b57996c9974691db296af0c8255
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-08-28 18:47:29 +0200

    Fix TitleH2 prop type  error

--- components/atom/SectionH2.tsx
+++ components/atom/SectionH2.tsx
@@ -1,5 +1,5 @@
 import {classed} from "@tw-classed/react"
-import {PropsWithChildren} from "react"
+import {MutableRefObject, PropsWithChildren, Ref} from "react"
 import {twMerge} from "tailwind-merge"
 
 // export const TitleH2 = classed(
@@ -8,14 +8,15 @@ import {twMerge} from "tailwind-merge"
 // 	twMerge("text-4xl sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl max-w-[25ch]")
 // )
 
-export const TitleH2 = ({children, className}: PropsWithChildren<{className?: string}>) => (
+export const TitleH2 = (props: PropsWithChildren<{className?: string; ref: MutableRefObject<any>}>) => (
 	<h2
 		className={twMerge(
 			"h2",
 			"font-poppins tracking-wider uppercase text-balance text-center text-primary",
 			" max-w-[25ch]",
-			className
-		)}>
-		{children}
+			props.className
+		)}
+		{...props}>
+		{props.children}
 	</h2>
 )
