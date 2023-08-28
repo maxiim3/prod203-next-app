c0c8850f3e91690b131b5ea43984433ef46e02c1
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-08-28 18:34:50 +0200

    added section with details in home page and adjusted layout

--- components/atom/AnimatedClientBanner.tsx
+++ components/atom/AnimatedClientBanner.tsx
@@ -1,38 +1,45 @@
-'use client'
+"use client"
 
-import {clients} from '@/lib/mocked_data'
-import {nanoid} from 'nanoid'
-import React from 'react'
-import Marquee from 'react-fast-marquee'
+import {clients} from "@/lib/mocked_data"
+import {nanoid} from "nanoid"
+import React from "react"
+import Marquee from "react-fast-marquee"
+import Image from "next/image"
 
 export function AnimatedClientBanner() {
-   return (
-      <div className={'md:hidden'}>
-         <Marquee
-            className={'flex w-screen items-center'}
-            speed={30}>
-            {clients.map(client => (
-               <figcaption key={nanoid()}>
-                  <img
-                     src={client.source}
-                     className={'mx-3 h-40 w-36 object-contain object-center'}
-                  />
-               </figcaption>
-            ))}
-         </Marquee>
-         <Marquee
-            direction={'right'}
-            speed={30}
-            className={'flex w-screen items-center'}>
-            {clients.reverse().map(client => (
-               <figcaption key={nanoid()}>
-                  <img
-                     src={client.source}
-                     className={'mx-3 h-40 w-36 object-contain object-center'}
-                  />
-               </figcaption>
-            ))}
-         </Marquee>
-      </div>
-   )
+	return (
+		<div className={"md:hidden"}>
+			<Marquee
+				className={"flex w-screen items-center"}
+				speed={30}>
+				{clients.map(client => (
+					<figcaption key={nanoid()}>
+						<Image
+							width={200}
+							height={200}
+							alt={client.title}
+							src={client.source}
+							className={"mx-3 h-40 w-36 object-contain object-center opacity-70"}
+						/>
+					</figcaption>
+				))}
+			</Marquee>
+			<Marquee
+				direction={"right"}
+				speed={30}
+				className={"flex w-screen items-center"}>
+				{clients.reverse().map(client => (
+					<figcaption key={nanoid()}>
+						<Image
+							width={200}
+							height={200}
+							alt={client.title}
+							src={client.source}
+							className={"mx-3 h-40 w-36 object-contain object-center opacity-70"}
+						/>
+					</figcaption>
+				))}
+			</Marquee>
+		</div>
+	)
 }
