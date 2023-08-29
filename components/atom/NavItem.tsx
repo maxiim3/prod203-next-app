91be6a245deb07982b9915a70c5b50c58d567f5c
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-08-29 15:31:43 +0200

    update homePage UI | Update Logo sizes | update header nav bar logo position and size | update overall margins in homepage | update service logo order

--- components/atom/NavItem.tsx
+++ components/atom/NavItem.tsx
@@ -1,26 +1,26 @@
-import {PropsWithChildren} from 'react'
-import {twMerge} from 'tailwind-merge'
+import {PropsWithChildren} from "react"
+import {twMerge} from "tailwind-merge"
 
 interface NavItemProps extends PropsWithChildren {
-   className?: string
-   active?: boolean
-   props?: any
+	className?: string
+	active?: boolean
+	props?: any
 }
 
 const NavItem = ({children, active, className, ...props}: NavItemProps) => (
-   <li
-      className={twMerge(
-         'relative text-xl font-thin uppercase tracking-wide text-primary',
-         // 'before:content[""] before:absolute before:-bottom-2 before:w-full before:origin-left before:scale-0 before:border before:border-primary/40',
-         'hover:text-primary/40',
-         'delay-[25ms] duration-200 before:duration-700 motion-safe:transition-all ',
-         // 'before:delay-75 hover:before:scale-100 motion-safe:before:transition-all',
-         active && 'underline underline-offset-2',
-         className
-      )}
-      {...props}>
-      {children}
-   </li>
+	<li
+		className={twMerge(
+			"relative text-xl font-thin uppercase tracking-wide text-primary",
+			// 'before:content[""] before:absolute before:-bottom-2 before:w-full before:origin-left before:scale-0 before:border before:border-primary/40',
+			"hover:text-primary/40",
+			"delay-[25ms] duration-200 before:duration-700 motion-safe:transition-all ",
+			// 'before:delay-75 hover:before:scale-100 motion-safe:before:transition-all',
+			active && "underline underline-offset-2",
+			className
+		)}
+		{...props}>
+		{children}
+	</li>
 )
 
 export default NavItem
