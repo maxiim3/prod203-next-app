929064f10444fe755e1f8fab003386ed44c1aa8e
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-08-30 13:34:20 +0200

    decide not to implement scroll snapping

---
+++ components/layout/SectionTemplate.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import {TitleH2} from '@/components/atom/SectionH2'
+import {motion} from 'framer-motion'
+import React, {PropsWithChildren} from 'react'
+import {twMerge} from 'tailwind-merge'
+
+export const SectionTemplate = ({
+   children,
+   outerContainerStyles,
+   innerContainerStyles,
+   sectionTitleStyles,
+   ariaLabel,
+   title,
+   id,
+}: PropsWithChildren<{
+   title: string
+   id: string
+   outerContainerStyles?: string
+   innerContainerStyles?: string
+   sectionTitleStyles?: string
+   ariaLabel?: string
+}>) => {
+   return (
+      <section
+         aria-label={ariaLabel}
+         className={twMerge(
+            ' relative mx-auto flex h-full min-h-screen w-screen max-w-[980px] snap-start items-center py-2 md:min-h-[75vh]',
+            outerContainerStyles
+         )}
+         id={id}>
+         <motion.main
+            initial={{opacity: 0}}
+            exit={{opacity: 1}}
+            whileInView={{opacity: 1}}
+            transition={{
+               duration: 0.65,
+               delay: 0.25,
+               type: 'spring',
+               stiffness: 260,
+               damping: 20,
+               staggerDirection: -1,
+            }}
+            className={twMerge(
+               'min-h-xl flex w-full flex-col  gap-12 overflow-hidden',
+               innerContainerStyles
+            )}>
+            <TitleH2
+               className={twMerge(
+                  'w-content mx-auto text-center text-4xl font-thin uppercase sm:text-5xl ',
+                  sectionTitleStyles
+               )}>
+               {title}
+            </TitleH2>
+            {children}
+         </motion.main>
+      </section>
+   )
+}
