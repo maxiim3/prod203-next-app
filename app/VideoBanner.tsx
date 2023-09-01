45b31fd40944447e0c8835e1bfca0af4a5d683ce
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-09-01 21:18:13 +0200

    Restore Video Banner codebase from PR

--- app/VideoBanner.tsx
+++ app/VideoBanner.tsx
@@ -1,11 +1,25 @@
 'use client'
 
+import {CldVideoPlayer} from 'next-cloudinary'
+import 'next-cloudinary/dist/cld-video-player.css'
 import React, {Suspense, useEffect, useRef, useState} from 'react'
+
+import useVideoSource, {VideoSource} from '@/hooks/useVideoSource'
 import {twMerge} from 'tailwind-merge'
 
 export function VideoBanner() {
-   const videoRef = useRef(null)
+   const VIDEO_SOURCE: VideoSource = Object.freeze({
+      LOW: 'https://res.cloudinary.com/dumtd7dhj/video/upload/v1693588665/prod203/prod203_capsule_herobanner_360p.webm',
+      MEDIUM:
+         'https://res.cloudinary.com/dumtd7dhj/video/upload/v1693588665/prod203/prod203_capsule_herobanner_480p.webm',
+      HIGH: 'https://res.cloudinary.com/dumtd7dhj/video/upload/v1693588665/prod203/prod203_capsule_herobanner_720p.webm',
+      VERY_HIGH:
+         'https://res.cloudinary.com/dumtd7dhj/video/upload/v1693588665/prod203/prod203_capsule_herobanner_1080p.webm',
+   })
+
+   const videoRef = useRef<HTMLVideoElement | null>(null)
    const [isMounted, setIsMounted] = useState(false)
+   const {setVideoSource} = useVideoSource(VIDEO_SOURCE, videoRef)
 
    useEffect(() => {
       setIsMounted(true)
@@ -15,58 +29,32 @@ export function VideoBanner() {
    }, [])
 
    useEffect(() => {
-      const videoElement = videoRef.current as null | HTMLVideoElement
-      const handleError = (e: any) => {
-         console.error('Video error', e)
-      }
-
-      if (videoElement) {
-         videoElement?.addEventListener('error', handleError)
-         videoElement?.load()
-         // console.log('Video element is Loaded')
-         videoElement?.play()
-      }
-
-      return () => {
-         if (videoElement) {
-            videoElement?.removeEventListener('error', handleError)
-         }
-      }
+      setVideoSource()
    }, [isMounted])
 
    return (
       <Suspense fallback={<p>Loading Video..</p>}>
-         <video
-            ref={videoRef}
-            autoPlay={true}
+         {/*<video*/}
+         {/*   ref={videoRef}*/}
+         {/*   autoPlay={true}*/}
+         {/*   muted={true}*/}
+         {/*   loop={true}*/}
+         {/*   playsInline*/}
+         {/*/>*/}
+         <CldVideoPlayer
+            width="1920"
+            videoRef={videoRef}
+            height="1080"
+            src="https://res.cloudinary.com/dumtd7dhj/video/upload/v1693588665/prod203/prod203_capsule_herobanner_720p.webm"
+            className={twMerge(
+               'h-[100vh] w-[100%] object-contain object-center',
+               'motion-safe:animate-[scaleAndFade_50ms_ease-out_both]'
+            )}
             muted={true}
             loop={true}
-            playsInline
-            className={twMerge(
-               'h-[100vh] w-[100%] object-cover object-center',
-               'motion-safe:animate-[scaleAndFade_50ms_ease-out_both]'
-            )}>
-            <source
-               src="/assets/video/pexels-c-technical-7095057%20(540p).mp4"
-               type="video/mp4"
-               media="(max-width: 680px)"
-            />
-            <source
-               src="/assets/video/pexels-c-technical-7095057%20(720p).mp4"
-               type="video/mp4"
-               media="(min-width: 680px) and (max-width: 1080px)"
-            />
-            <source
-               src="/assets/video/pexels-c-technical-7095057%20(1080p).mp4"
-               type="video/mp4"
-               media="(min-width: 1080px) and (max-width: 1439px)"
-            />
-            <source
-               src="/assets/video/pexels-c-technical-7095057%20(2160p).mp4"
-               type="video/mp4"
-               media="(min-width: 1440px)"
-            />
-         </video>
+            controls={false}
+            quality={'auto'}
+         />
       </Suspense>
    )
 }
