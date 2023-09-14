687cc5d765810894ed2beda251355a9d56ea2f94
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-09-14 17:41:36 +0200

    removed conflicting type

--- app/(pages)/[lang]/home/page.tsx
+++ app/(pages)/[lang]/home/page.tsx
@@ -1,7 +1,7 @@
-import {BuildingPage} from '@/components/ui/BuildingPage'
+import { BuildingPage } from '@/components/ui/BuildingPage'
 import React from 'react'
 
-const Page = ({params}: {params: {lang: 'fr' | 'eng'}}) => {
+const Page = ({ params }: { params: { lang: 'fr' | 'eng' } }) => {
    return (
       <main className={'flex h-screen w-screen items-center justify-center'}>
          <div className={'mx-auto flex w-96 flex-col gap-12'}>
@@ -18,11 +18,11 @@ const pageContent = {
    title: {
       eng: 'This page is in : ',
       fr: 'Cette Page est en : ',
-   } satisfies International,
+   },
    content: {
       eng: 'Life is a journey full of twists and turns, so embrace the unknown and never stop learning.',
       fr: "La vie est un voyage plein de rebondissements, alors accueillez l'inconnu et ne cessez jamais d'apprendre.",
-   } satisfies International,
+   },
 }
 
 export default Page
