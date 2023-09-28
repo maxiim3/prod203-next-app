768e3de7ea030a65dea91075c8d60da7e226dbff
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-09-28 11:40:38 +0200

    image not displaying because of middleware.ts : Updating the matcher to bypass the assets from public/

--- lib/secrets.env.ts
+++
@@ -1,6 +0,0 @@
-const secretsEnv = Object.freeze({
-   sanity_project_id: process.env.SANITY_PROJECT_ID,
-   cloudinary_cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
-})
-
-export default secretsEnv
