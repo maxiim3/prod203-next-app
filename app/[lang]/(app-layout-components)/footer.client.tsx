e97355fbb095372ea3a4df0e9f0aa688b19c8da6
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-10-05 16:30:42 +0200

    Updated footer.client.tsx

--- app/[lang]/(app-layout-components)/footer.client.tsx
+++ app/[lang]/(app-layout-components)/footer.client.tsx
@@ -3,6 +3,7 @@
 import useLangParamsHook from '@/hooks/useLangParams.hook'
 import {cn} from '@/lib/utils'
 import routes from '@/static-content/route.static.content'
+import {Github} from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React, {ComponentPropsWithoutRef, PropsWithChildren} from 'react'
@@ -104,8 +105,42 @@ export default function Footer() {
                      </Link>
                   </Item>
                </Section>
+               <Section>
+                  <SectionTitle>Design & Developpement</SectionTitle>
+                  <Item className={'flex items-center gap-1'}>
+                     <Link href={'https://github.com/maxiim3/prod203-next-app'}>
+                        <Github className={'w-4'} />
+                     </Link>
+                     Maxime Tamburrini
+                  </Item>
+                  <Item className={'flex items-center gap-1'}>
+                     <svg
+                        className={'fill-primary opacity-60'}
+                        version="1.0"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        viewBox="0 0 105.000000 102.000000"
+                        preserveAspectRatio="xMidYMid meet">
+                        <g
+                           transform="translate(0.000000,102.000000) scale(0.100000,-0.100000)"
+                           stroke="none">
+                           <path
+                              d="M0 721 l0 -299 322 155 c315 151 322 154 340 135 19 -18 12 -22 -322
+-201 l-340 -183 0 -114 c0 -63 3 -114 6 -114 4 0 401 301 701 532 8 6 15 3 21
+-9 9 -15 -39 -61 -335 -318 l-345 -300 288 -3 c264 -2 295 -1 354 17 156 48
+279 169 337 331 28 80 31 218 7 301 -42 143 -144 261 -281 327 l-78 37 -337 3
+-338 3 0 -300z m535 60 c15 -6 0 -14 -70 -35 -49 -15 -103 -31 -119 -36 -44
+-15 -46 -13 -46 35 l0 45 108 -1 c59 0 116 -4 127 -8z m194 -380 c-12 -28 -39
+-67 -61 -87 -38 -34 -125 -81 -134 -71 -2 2 44 63 103 136 102 124 108 130
+111 102 2 -16 -7 -52 -19 -80z"
+                           />
+                        </g>
+                     </svg>
+                     Digital Solution Studio
+                  </Item>
+               </Section>
             </div>
-
             <p className="text-xs text-primary/70">
                &copy; 2023. Jamais 203 Productions. All rights reserved.
             </p>
--- app/[lang]/legal/page.tsx
+++ app/[lang]/legal/page.tsx
@@ -1,91 +1,132 @@
-import SinglePageLayoutTemplate from '@/components/single-page-layout.template'
+import {cn} from '@/lib/utils'
 import {I_PageI18nParams} from '@/schemas/i18n.page.props.schema'
+import Link from 'next/link'
+import React, {ComponentPropsWithoutRef, ComponentPropsWithRef} from 'react'
 
 export default function Legal({params}: I_PageI18nParams) {
    const {lang} = params
 
-   return (
-      <SinglePageLayoutTemplate
-         className={
-            'text-poppins text-prirmary prose mx-auto flex h-auto w-screen flex-col items-center justify-center px-2 py-32 font-light text-balance md:px-12'
-         }>
-         {lang === 'en' ? (
-            <>
-               <h1>LEGAL NOTICE</h1>
-               <p>
-                  The company JAMAIS 203 PRODUCTIONS, in operation since 15/10/2021, with the
-                  capital of 3000€, registered at the RCS of La Roche-sur-Yon under the number 904
-                  632 320, with the SIRET 90463232000019, whose registered office is located at
-                  COURAUX, 85700 POUZAUGES.
-               </p>
-               <p>Hosting: Vercel, address and contact number of Vercel.</p>
+   const Text = ({children, className}: ComponentPropsWithoutRef<'p'>) => (
+      <p className={cn('m-0 p-0 font-light !text-primary text-balance', className)}>{children}</p>
+   )
+   const Main = ({className, children}: ComponentPropsWithRef<'main'>) => (
+      <main
+         className={cn(
+            'prose mx-auto mb-12 mt-24 flex min-h-screen w-screen flex-col items-center justify-center gap-12 text-sm sm:text-base',
+            className
+         )}>
+         {children}
+      </main>
+   )
+
+   const Block = ({children, className}: ComponentPropsWithoutRef<'div'>) => (
+      <div className={cn('prose flex max-w-[85ch] flex-col gap-2', className)}>{children}</div>
+   )
+
+   if (lang === 'en')
+      return (
+         <Main>
+            <Block>
+               <h1>LEGAL MENTIONS</h1>
+               <Text>
+                  The company NEVER 203 PRODUCTIONS, active since 10/15/2021, with a capital of
+                  €3000, registered with the RCS of La Roche-sur-Yon under the number 904 632 320,
+                  with the SIRET 90463232000019, whose registered office is located at COURAUX,
+                  85700 POUZAUGES.
+               </Text>
+               <Text>Host: Vercel, address and contact number of Vercel.</Text>
+            </Block>
+            <Block>
                <h2>PERSONAL DATA</h2>
                <h3>PERSONAL DATA - PRIVACY POLICY</h3>
-               <p>
-                  In accordance with the amended Data Protection Act of January 6, 1978, you have
-                  the right to access, modify, and delete personal data concerning you. No personal
-                  information is collected without your knowledge. No personal information is given
-                  to third parties. You can exercise this right by specifying your first name, last
-                  name, and complete address, either: By email to the address: [Your email address]
-                  By mail to the following address: [Your postal address]
-               </p>
-               <p>No cookies or personal data are collected on this site.</p>
+               <Text>
+                  In application of the modified data protection act of January 6, 1978, you have
+                  the right to access, modify, and withdraw personal data concerning you. No
+                  personal information is collected without your knowledge. No personal information
+                  is given to third parties. You can exercise this right by providing your first
+                  name, last name, and full address, either: By email at the address:
+                  <Link
+                     type={'mail'}
+                     href="mailto:contact@prod203.com">
+                     contact@prod203.com
+                  </Link>
+                  . By postal mail at the following address: COURAUX, 85700 POUZAUGES.
+               </Text>
+               <Text>No cookies or personal data are collected on this site.</Text>
+            </Block>
+            <Block>
                <h2>INTELLECTUAL PROPERTY</h2>
-               <p>
+               <Text>
                   The site and all of its content are subject to French and international copyright
-                  and intellectual property laws and are the exclusive property of NEVER 203
-                  PRODUCTIONS and/or its partners. Unless prior and explicit authorization from
-                  NEVER 203 PRODUCTIONS, the representation or reproduction of all or part of this
-                  site is strictly prohibited.
-               </p>
+                  and intellectual property laws, and are the exclusive property of NEVER 203
+                  PRODUCTIONS and/or its partners. Unless prior and express authorization from NEVER
+                  203 PRODUCTIONS, the representation or reproduction of all or part of this site is
+                  strictly prohibited.
+               </Text>
+            </Block>
+            <Block>
                <h2>APPLICABLE LAW AND JURISDICTION</h2>
-               <p>
-                  These legal notices and privacy policy are governed by French law. In case of
-                  dispute concerning their validity, interpretation, and/or application, express
-                  jurisdiction is granted to the competent courts of [Your jurisdiction, often the
-                  city where the head office is located].
-               </p>
-            </>
-         ) : (
-            <>
-               <h1>MENTIONS LÉGALES</h1>
-               <p>
-                  La société JAMAIS 203 PRODUCTIONS, en activité depuis le 15/10/2021, au capital de
-                  3000€, immatriculée au RCS de La Roche-sur-Yon sous le numéro 904 632 320, avec le
-                  SIRET 90463232000019, dont le siège social est situé à COURAUX, 85700 POUZAUGES.
-               </p>
-               <p>Hébergeur: Vercel, adresse et numéro de contact de Vercel.</p>
-               <h2>DONNÉES PERSONNELLES</h2>
-               <h3>DONNEES PERSONNELLES - POLITIQUE DE CONFIDENTIALITÉ</h3>
-               <p>
-                  En application de la loi informatique et liberté du 6 janvier 1978 modifiée, vous
-                  disposez d’un droit d’accès, de rectification et de retrait des données
-                  personnelles vous concernant. Aucune information personnelle n&apos;est collectée
-                  à votre insu. Aucune information personnelle n&apos;est cédée à des tiers. Vous
-                  pouvez exercer ce droit en indiquant vos nom, prénom et adresse complète, soit :
-                  Par courrier électronique à l’adresse : [Votre adresse e-mail] Par voie postale à
-                  l’adresse suivante : [Votre adresse postale]
-               </p>
-               Aucun cookie ni données
-               <p>personnelles ne sont collectés sur ce site</p>
-               <h2>PROPRIÉTÉ INTELLECTUELLE</h2>
-               <p>
-                  Le site et l’ensemble de son contenu relèvent de la législation française et
-                  internationale sur le droit d’auteur et la propriété intellectuelle, et sont la
-                  propriété exclusive de JAMAIS 203 PRODUCTIONS et/ou de ses partenaires. Sauf
-                  autorisation préalable et expresse de JAMAIS 203 PRODUCTIONS, la représentation ou
-                  la reproduction de tout ou partie de ce site est formellement interdite.
-               </p>
-               <h2>LOI APPLICABLE ET COMPÉTENCE JURIDICTIONNELLE</h2>
-               <p>
-                  Les présentes mentions légales et politique de confidentialité sont régies par la
-                  loi française. En cas de litige relatif à leur validité, leur interprétation et/ou
-                  leur application, attribution expresse de juridiction est donnée aux tribunaux
-                  compétents de [Votre juridiction, souvent la ville où est situé le siège social].
-               </p>
-            </>
-         )}
-      </SinglePageLayoutTemplate>
+               <Text>
+                  These legal notices and privacy policy are governed by French law. In the event of
+                  a dispute relating to their validity, interpretation, and/or application, express
+                  jurisdiction is granted to the competent courts of POUZAUGES.
+               </Text>
+            </Block>
+         </Main>
+      )
+   return (
+      <Main>
+         <Block>
+            <h1>MENTIONS LÉGALES</h1>
+            <Text>
+               La société JAMAIS 203 PRODUCTIONS, en activité depuis le 15/10/2021, au capital de
+               3000€, immatriculée au RCS de La Roche-sur-Yon sous le numéro 904 632 320, avec le
+               SIRET 90463232000019, dont le siège social est situé à COURAUX, 85700 POUZAUGES.
+            </Text>
+            <Text>
+               Hébergeur: <Link href={'https://vercel.com/home'}>Vercel Inc.</Link> 440 N Barranca
+               Ave #4133 Covina, CA 91723
+            </Text>
+         </Block>
+         <Block>
+            <h2>DONNÉES PERSONNELLES</h2>
+            <h3>DONNEES PERSONNELLES - POLITIQUE DE CONFIDENTIALITÉ</h3>
+            <Text>
+               En application de la loi informatique et liberté du 6 janvier 1978 modifiée, vous
+               disposez d’un droit d’accès, de rectification et de retrait des données personnelles
+               vous concernant. Aucune information personnelle n&apos;est collectée à votre insu.
+               Aucune information personnelle n&apos;est cédée à des tiers. Vous pouvez exercer ce
+               droit en indiquant vos nom, prénom et adresse complète, soit : Par courrier
+               électronique à l’adresse :{' '}
+               <Link
+                  type={'mail'}
+                  href="mailto:contact@prod203.com">
+                  contact@prod203.com
+               </Link>
+               {'. '}Par voie postale à l’adresse suivante : COURAUX, 85700 POUZAUGES.
+            </Text>
+            <Text>Aucun cookie ni données personnelles ne sont collectés sur ce site</Text>
+         </Block>
+         <Block>
+            <h2>PROPRIÉTÉ INTELLECTUELLE</h2>
+            <Text>
+               Le site et l’ensemble de son contenu relèvent de la législation française et
+               internationale sur le droit d’auteur et la propriété intellectuelle, et sont la
+               propriété exclusive de JAMAIS 203 PRODUCTIONS et/ou de ses partenaires. Sauf
+               autorisation préalable et expresse de JAMAIS 203 PRODUCTIONS, la représentation ou la
+               reproduction de tout ou partie de ce site est formellement interdite.
+            </Text>
+         </Block>
+         <Block>
+            <h2>LOI APPLICABLE ET COMPÉTENCE JURIDICTIONNELLE</h2>
+            <Text>
+               Les présentes mentions légales et politique de confidentialité sont régies par la loi
+               française. En cas de litige relatif à leur validité, leur interprétation et/ou leur
+               application, attribution expresse de juridiction est donnée aux tribunaux compétents
+               de POUZAUGES.
+            </Text>
+         </Block>
+      </Main>
    )
 }
 // # Identification du professionnel :
