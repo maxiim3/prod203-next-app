ff98b277eaa0be47a8375c98fb2a35c42675e95d
Maxime Tamburrini <maximemickael.contact@gmail.com>
2023-11-01 15:04:57 +0100

    Remove extra "." from activities paragraphs

--- app/[lang]/about/(ui)/organisms/section-activity.client.tsx
+++ app/[lang]/about/(ui)/organisms/section-activity.client.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import {Activities, Activity} from '@/app/[lang]/about/(factories)/activity.factory'
+import {Activity} from '@/app/[lang]/about/(factories)/activity.factory'
 import {useCarousel} from '@/app/[lang]/about/(hooks)/use-carousel.hook'
 import type {T_ActivityStore} from '@/app/[lang]/about/(store)/store.types'
 import {useActivityTabsStore} from '@/app/[lang]/about/(store)/use-activity-tabs.store'
@@ -14,7 +14,12 @@ import Image from 'next/image'
 import React from 'react'
 
 export default function SectionActivity() {
-   const activityStore: T_ActivityStore = useActivityTabsStore((store: T_ActivityStore) => store)
+   const activeTab: T_ActivityStore['activeTab'] = useActivityTabsStore(
+      (store: T_ActivityStore) => store.activeTab
+   )
+   const getALlActivities: T_ActivityStore['getAll'] = useActivityTabsStore(
+      (store: T_ActivityStore) => store.getAll
+   )
    const carouselRef = useCarousel()
    const {lang} = useLangParams()
 
@@ -37,17 +42,17 @@ export default function SectionActivity() {
                'carousel carousel-center relative w-screen cursor-grab space-x-4 p-4 active:cursor-grabbing sm:space-x-6 sm:p-6 md:space-x-8 md:p-8 xl:w-full xl:px-24'
             }>
             <>
-               {[...Activities.entries()].map(([key, activity], index) => {
+               {[...getALlActivities().entries()].map(([key, activity], activityIndex) => {
                   const {title, description, Icon, source}: Activity = activity
                   const activityTitle = title[lang]!
                   const activityDesc = description[lang]!
 
                   return (
                      <CarouselItemContainer
-                        key={`card-${index}`}
+                        key={`card-${activityIndex}`}
                         tab={key}>
                         <header
-                           key={`tab-title-${index}-${key}`}
+                           key={`tab-title-${activityIndex}-${key}`}
                            className={'relative border-b border-primary/60 pb-2'}>
                            <Pictogram
                               activityIcon={Icon}
@@ -64,16 +69,20 @@ export default function SectionActivity() {
                            className={
                               '  flex flex-col items-center justify-center gap-2 px-2 md:px-6'
                            }>
-                           {activityDesc.split('. ').map((content, index) => (
-                              <TabText
-                                 className={
-                                    'm-0 w-full max-w-[80ch] text-center font-poppins text-xs font-light !text-primary text-balance sm:text-sm'
-                                 }
-                                 key={`tab-content-${key}-${index}`}>
-                                 {content}
-                                 {index !== Object.entries(Activities).length - 1 && '.'}
-                              </TabText>
-                           ))}
+                           {activityDesc.split('. ').map((content, paragraphIndex) => {
+                              const maxIndex: number = activityDesc.split('. ').length - 1
+
+                              return (
+                                 <TabText
+                                    className={
+                                       'm-0 w-full max-w-[80ch] text-center font-poppins text-xs font-light !text-primary text-balance sm:text-sm'
+                                    }
+                                    key={`tab-content-${key}-${paragraphIndex}`}>
+                                    {content}
+                                    {paragraphIndex !== maxIndex && '.'}
+                                 </TabText>
+                              )
+                           })}
                         </main>
                      </CarouselItemContainer>
                   )
@@ -83,12 +92,12 @@ export default function SectionActivity() {
          <footer className={'absolute bottom-3 flex w-full items-center justify-center'}>
             <div className={' mx-auto flex gap-2 '}>
                <>
-                  {[...Activities.entries()].map(([key, activity], index) => (
+                  {[...getALlActivities().entries()].map(([key, activity], index) => (
                      <span
                         key={index}
                         className={cn(
                            'h-4 w-4 rounded-full ',
-                           activityStore.activeTab === key ? 'bg-primary' : 'border border-primary'
+                           activeTab === key ? 'bg-primary' : 'border border-primary'
                         )}
                      />
                   ))}
